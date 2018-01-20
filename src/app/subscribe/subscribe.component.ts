d8d5dd52c99f38897a229c6cc0bf93ee2cd044e0
Guillon <guillonangele@gmail.com>
2018-01-20 10:21:42 +0100

    login fini mais a tester

--- src/app/subscribe/subscribe.component.ts
+++ src/app/subscribe/subscribe.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material';
+import { Router } from '@angular/router';
+import { UserService, AlertService } from '../_services/index';
 
 @Component({
   selector: 'app-subscribe',
@@ -8,18 +10,36 @@ import { MatTableDataSource } from '@angular/material';
 })
 export class SubscribeComponent implements OnInit {
 
-  constructor() { }
-
   factions: Factions[] = [
     {id: 1, name: 'Shadow'},
     {id: 2, name: 'Light'},
   ];
+  
+  model: any = {};
+  loading = false;
 
-  ngOnInit() {
+  constructor(
+      private router: Router,
+      private userService: UserService,
+      private alertService: AlertService) { }
 
+  register() {
+      this.loading = true;
+      this.userService.create(this.model)
+          .subscribe(
+              data => {
+                  this.alertService.success('Registration successful', true);
+                  this.router.navigate(['/login']);
+              },
+              error => {
+                  this.alertService.error(error);
+                  this.loading = false;
+              });
   }
+  ngOnInit() {}
 
 }
+
 export interface Factions {
   name: string ;
   id: number ;
